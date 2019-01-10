24922c545fbc3234dd99599c268c028a801efe93
zmij <sergei.a.fedorov@gmail.com>
2019-01-10 13:29:06 +0300

    feat: add some more tests

--- test/random_tests.cpp
+++ test/random_tests.cpp
@@ -25,18 +25,25 @@ using vector4f = vector<float, 4>;
 
 TEST(Vector, GenerateRandom)
 {
-    vector3d v1 = random_vector_data<double>(std::uniform_real_distribution<double>{0, 1});
+    auto gen_d = random_vector_data<double>(std::uniform_real_distribution<double>{0, 1});
+    auto gen_f = random_vector_data<float>(std::normal_distribution<float>{0, 1});
+
+    vector3d v1 = gen_d;
     std::cout << "Random vector " << v1 << "\n";
     EXPECT_NE(0, magnitude_square(v1));
-    vector4d v2 = random_vector_data<double>(std::uniform_real_distribution<double>{0, 1});
+    v1 = gen_d;
+    std::cout << "Random vector " << v1 << "\n";
+    EXPECT_NE(0, magnitude_square(v1));
+
+    vector4d v2 = gen_d;
     std::cout << "Random vector " << v2 << "\n";
     EXPECT_NE(0, magnitude_square(v2));
 
-    vector3f v3 = random_vector_data<float>(std::normal_distribution<float>{0, 1});
+    vector3f v3 = gen_f;
     std::cout << "Random vector " << v3 << "\n";
     EXPECT_NE(0, magnitude_square(v3));
 
-    vector4f v4 = random_vector_data<float>(std::normal_distribution<float>{0, 1});
+    vector4f v4 = gen_f;
     std::cout << "Random vector " << v4 << "\n";
     EXPECT_NE(0, magnitude_square(v4));
 }
