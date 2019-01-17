99a726404b45a14aec035631414efbabe5acd3fe
zmij <sergei.a.fedorov@gmail.com>
2019-01-17 22:48:28 +0300

    feat: memory vector view utility

--- include/pushkin/math/vector_view.hpp
+++ include/pushkin/math/vector_view.hpp
@@ -11,8 +11,12 @@
 
 #include <pushkin/math/detail/vector_expressions.hpp>
 
+#include <iterator>
+
 namespace psst {
 namespace math {
+
+// TODO Deal with alignment stuff
 
 // Mutating vector_view
 template <typename T, std::size_t Size, typename Axes>
@@ -40,6 +44,15 @@ struct vector_view<T*, Size, Axes>
     static constexpr auto size = traits::size;
 
     constexpr explicit vector_view(pointer p) : data_{p} {}
+
+    template <typename Expression, typename = enable_if_vector_expression<Expression>,
+              typename = enable_for_compatible_axes<this_type, Expression>>
+    vector_view&
+    operator=(Expression const& rhs)
+    {
+        return assign(rhs,
+                      utils::make_min_index_sequence<Size, vector_expression_size_v<Expression>>{});
+    }
 
     pointer
     data()
@@ -131,6 +144,15 @@ struct vector_view<T*, Size, Axes>
     operator const_pointer() const { return data(); }
 
 private:
+    template <typename Expr, std::size_t... Indexes>
+    vector_view&
+    assign(Expr&& rhs, std::index_sequence<Indexes...>)
+    {
+        ((this->template at<Indexes>() = rhs.template at<Indexes>()), ...);
+        return *this;
+    }
+
+private:
     pointer data_;
 };
 
@@ -219,17 +241,236 @@ private:
     const_pointer data_;
 };
 
-template <std::size_t Size, typename Axes = axes::default_axes_t<Size>, typename T>
+/**
+ * Utility to treat a region of memory as a 'container' of vectors of certain type
+ */
+template <typename T, std::size_t Size, typename Axes = axes::default_axes_t<Size>>
+struct memory_vector_view;
+
+template <typename T, std::size_t Size, typename Axes>
+struct memory_vector_view<T*, Size, Axes> {
+    using pointer_type       = T*;
+    using const_pointer_type = T const*;
+    using view_type          = vector_view<T*, Size, Axes>;
+
+    static constexpr std::size_t component_count = Size;
+    static constexpr std::size_t element_size    = sizeof(T) * component_count;
+
+    template <typename P>
+    struct base_iterator : std::iterator<std::random_access_iterator_tag, view_type, std::ptrdiff_t,
+                                         view_type, view_type> {
+        using base_type = std::iterator<std::random_access_iterator_tag, view_type, std::ptrdiff_t,
+                                        view_type, view_type>;
+        using difference_type = typename base_type::difference_type;
+        using value_type      = typename base_type::value_type;
+
+        base_iterator(P p) : p_{p} {}
+
+        bool
+        operator==(base_iterator const& rhs) const
+        {
+            return p_ == rhs.p_;
+        }
+        bool
+        operator!=(base_iterator const& rhs) const
+        {
+            return p_ != rhs.p_;
+        }
+
+        base_iterator&
+        operator++()
+        {
+            p_ += component_count;
+            return *this;
+        }
+        base_iterator
+        operator++(int)
+        {
+            base_iterator i{p_};
+            p_ += component_count;
+            return i;
+        }
+        base_iterator
+        operator+(difference_type d) const
+        {
+            return base_iterator{p_ + d * component_count};
+        }
+        base_iterator&
+        operator+=(difference_type d)
+        {
+            p_ += d * component_count;
+            return *this;
+        }
+
+        base_iterator&
+        operator--()
+        {
+            p_ -= component_count;
+            return *this;
+        }
+        base_iterator
+        operator--(int)
+        {
+            base_iterator i{p_};
+            p_ -= component_count;
+            return i;
+        }
+        base_iterator
+        operator-(difference_type d) const
+        {
+            return base_iterator{p_ - d * component_count};
+        }
+        difference_type
+        operator-(base_iterator const& rhs) const
+        {
+            return (p_ - rhs.p_) / component_count;
+        }
+        base_iterator&
+        operator-=(difference_type d)
+        {
+            p_ -= d * component_count;
+            return *this;
+        }
+
+        base_iterator operator[](difference_type index) const
+        {
+            return base_iterator{p_ + index * component_count};
+        }
+
+        value_type operator*() const { return value_type{p_}; }
+        value_type operator->() const { return value_type{p_}; }
+
+    private:
+        P p_;
+    };
+
+    using iterator       = base_iterator<pointer_type>;
+    using const_iterator = base_iterator<const_pointer_type>;
+
+    constexpr memory_vector_view(pointer_type buffer, std::size_t buffer_size)
+        : buffer_{buffer}, buffer_size_{buffer_ ? buffer_size : 0}
+    {
+        if (buffer_size % Size != 0)
+            throw std::runtime_error{"The size of buffer is not a multiple of components"};
+    }
+
+    /**
+     * Is the memory empty
+     * @return
+     */
+    constexpr bool
+    empty() const
+    {
+        return buffer_size_ == 0;
+    }
+
+    /**
+     * Number of vector_view elements available
+     * @return
+     */
+    constexpr std::size_t
+    size() const
+    {
+        return buffer_size_ / Size;
+    }
+
+    constexpr view_type operator[](std::size_t index) const
+    {
+        return view_type{buffer_ + index * element_size};
+    }
+
+    constexpr iterator
+    begin()
+    {
+        return iterator{buffer_};
+    }
+    constexpr const_iterator
+    begin() const
+    {
+        return cbegin();
+    }
+    constexpr const_iterator
+    cbegin() const
+    {
+        return const_iterator{buffer_};
+    }
+
+    constexpr iterator
+    end()
+    {
+        return iterator{buffer_ + buffer_size_};
+    }
+    constexpr const_iterator
+    end() const
+    {
+        return cend();
+    }
+    constexpr const_iterator
+    cend() const
+    {
+        return const_iterator{buffer_ + buffer_size_};
+    }
+
+private:
+    pointer_type buffer_;
+    std::size_t  buffer_size_;
+};
+
+//----------------------------------------------------------------------------
+template <typename T, typename U, typename = enable_if_vector<T>>
 constexpr auto
-make_vector_view(T* val)
+make_vector_view(U* buffer)
 {
-    return vector_view<T*, Size, Axes>(val);
+    using value_type    = scalar_expression_result_t<T>;
+    using axes_type     = axes_names_t<T>;
+    constexpr auto size = vector_expression_size_v<T>;
+    static_assert((std::is_same<std::decay_t<U>, value_type>{}), "Incompatible pointer type");
+    return vector_view<U*, size, axes_type>(buffer);
 }
-template <std::size_t Size, typename Axes = axes::default_axes_t<Size>, typename T>
+
+template <typename T, typename = enable_if_vector<T>>
 constexpr auto
-make_vector_view(T const* val)
+make_vector_view(char* buffer)
 {
-    return vector_view<T const*, Size, Axes>(val);
+    using value_type = scalar_expression_result_t<T>;
+    return make_vector_view<T>(reinterpret_cast<value_type*>(buffer));
+}
+
+template <typename T, typename = enable_if_vector<T>>
+constexpr auto
+make_vector_view(char const* buffer)
+{
+    using value_type = scalar_expression_result_t<T>;
+    return make_vector_view<T>(reinterpret_cast<value_type const*>(buffer));
+}
+
+template <typename T, typename U, typename = enable_if_vector<T>>
+constexpr auto
+make_memory_vector_view(U* val, std::size_t buffer_size)
+{
+    using value_type    = scalar_expression_result_t<T>;
+    using axes_type     = axes_names_t<T>;
+    constexpr auto size = vector_expression_size_v<T>;
+    static_assert((std::is_same<std::decay_t<U>, value_type>{}), "Incompatible pointer type");
+    return memory_vector_view<U*, size, axes_type>(val, buffer_size);
+}
+
+template <typename T, typename = enable_if_vector<T>>
+constexpr auto
+make_memory_vector_view(char* buffer, std::size_t buffer_size)
+{
+    using value_type = scalar_expression_result_t<T>;
+    return make_memory_vector_view<T>(reinterpret_cast<value_type*>(buffer),
+                                      buffer_size / sizeof(value_type));
+}
+
+template <typename T, typename = enable_if_vector<T>>
+constexpr auto
+make_memory_vector_view(char const* buffer, std::size_t buffer_size)
+{
+    using value_type = scalar_expression_result_t<T>;
+    return make_memory_vector_view<T>(reinterpret_cast<value_type const*>(buffer),
+                                      buffer_size / sizeof(value_type));
 }
 
 }    // namespace math
--- test/vector_view_tests.cpp
+++ test/vector_view_tests.cpp
@@ -13,6 +13,7 @@
 #include <gtest/gtest.h>
 
 #include <sstream>
+#include <vector>
 
 namespace psst {
 namespace math {
@@ -92,16 +93,81 @@ TEST(VectorView, View)
 
 TEST(VectorView, ViewModification)
 {
-    {
-        vector3f      v{1, 2, 3};
-        vector3f_view vv{v.data()};
+    vector3f      v1{1, 2, 3};
+    vector3f      v2{4, 5, 6};
+    vector3f_view vv{v1.data()};
 
-        vv[0] = 2;
-        EXPECT_EQ(2, v[0]);
-        vv.at<0>() = 1;
-        EXPECT_EQ(1, v.at<0>());
-        vv.x() = 3;
-        EXPECT_EQ(3, v.x());
+    EXPECT_NE(v2, v1);
+    EXPECT_NE(v2, vv);
+
+    vv[0] = 2;
+    EXPECT_EQ(2, v1[0]);
+    vv.at<0>() = 1;
+    EXPECT_EQ(1, v1.at<0>());
+    vv.x() = 3;
+    EXPECT_EQ(3, v1.x());
+
+    vv = v2;
+    EXPECT_EQ(v2, v1);
+    EXPECT_EQ(v2, vv);
+
+    vv = v2 + v2;
+    EXPECT_EQ(v2 * 2, v1);
+    EXPECT_EQ(v2 * 2, vv);
+}
+
+TEST(VectorView, AccessBuffers)
+{
+    {
+        vector3f v1{1, 2, 3};
+        auto     vv = make_vector_view<vector3f>(v1.data());
+        EXPECT_EQ(v1, vv);
+    }
+    {
+        std::vector<vector3f> vectors{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        float const* float_const_buf = vectors.data()->data();
+        auto const   float_buf_size  = vector3f::size * vectors.size();
+        auto mem_const_view = make_memory_vector_view<vector3f>(float_const_buf, float_buf_size);
+
+        EXPECT_EQ(vectors.size(), mem_const_view.size());
+        std::size_t i = 0;
+        for (auto v : mem_const_view) {
+            EXPECT_EQ(vectors[i], v);
+            ++i;
+        }
+        float* float_buf = vectors.data()->data();
+        auto   mem_view  = make_memory_vector_view<vector3f>(float_buf, float_buf_size);
+        EXPECT_EQ(vectors.size(), mem_view.size());
+        i = 0;
+        for (auto v : mem_view) {
+            EXPECT_EQ(vectors[i], v);
+            v.z() = 42;
+            EXPECT_EQ(42, vectors[i].z());
+            ++i;
+        }
+
+        char const* char_const_buf = reinterpret_cast<char const*>(float_const_buf);
+        auto const  char_buf_size  = float_buf_size * sizeof(float);
+        auto char_mem_const_view = make_memory_vector_view<vector3f>(char_const_buf, char_buf_size);
+        EXPECT_EQ(vectors.size(), char_mem_const_view.size());
+        i = 0;
+        for (auto v : char_mem_const_view) {
+            EXPECT_EQ(vectors[i], v);
+            ++i;
+        }
+
+        char* char_buf      = reinterpret_cast<char*>(float_buf);
+        auto  char_mem_view = make_memory_vector_view<vector3f>(char_buf, char_buf_size);
+        EXPECT_EQ(vectors.size(), char_mem_view.size());
+        i = 0;
+        for (auto v : char_mem_view) {
+            EXPECT_EQ(vectors[i], v);
+            EXPECT_EQ(42, v.z());
+            v.y() = 42;
+            EXPECT_EQ(42, vectors[i].y());
+            ++i;
+        }
     }
 }
 
